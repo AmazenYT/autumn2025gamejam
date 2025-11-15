841aedbad84feedb215191fd0a4ea1374aa514ff
Michael4Camps <C24354791@myTUDublin.ie>
2025-11-15 16:35:58 +0000

    Working Sword!!?!!
    Needs animation

---
+++ Autumn GameJam/Assets/SwordSwing.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class SwordSwing : MonoBehaviour
+{
+    public int damage = 25;
+
+    public Transform attackPoint;
+    public float attackRange = 10;
+
+    public LayerMask enemyLayers;
+    void Update()
+    {
+            
+    if (Input.GetMouseButtonDown(0))
+    {
+        Debug.Log("Attack");
+        Attack();
+    }
+        
+    }
+
+    public void Attack()
+    {
+        Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider enemy in hitEnemies)
+            {
+                Debug.Log("Enemy hit with sword");
+                enemy.GetComponent<EnemyScript>().TakeDamage(damage);
+            }
+        
+    }
+}
